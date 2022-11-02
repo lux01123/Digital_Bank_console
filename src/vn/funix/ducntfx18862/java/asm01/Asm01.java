6a2c729b3d6dc4b7fd1f0c079741ad6d0c8c033b
lux01123 <nguyenduc27315@gmail.com>
2022-11-02 09:42:21 +0700

    Finished V2.1

--- src/vn/funix/ducntfx18862/java/asm01/Asm01.java
+++ src/vn/funix/ducntfx18862/java/asm01/Asm01.java
@@ -1,3 +1,4 @@
+//Version: Java 8 - JDK 18.0.2
 package vn.funix.ducntfx18862.java.asm01;
 
 import java.util.Scanner;
@@ -14,14 +15,23 @@ public class Asm01 {
         return ranNum;
     }
 
-    // Create string security number
+    // Create string security number ADVANTAGE
     public static String CreateStringNumber() {
         String securityString = null;
+        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random r = new Random();
+        char c1 = alphabet.charAt(r.nextInt(alphabet.length()));
+        char c2 = alphabet.charAt(r.nextInt(alphabet.length()));
+        char c3 = alphabet.charAt(r.nextInt(alphabet.length()));
+        char c4 = alphabet.charAt(r.nextInt(alphabet.length()));
+        char c5 = alphabet.charAt(r.nextInt(alphabet.length()));
+        char c6 = alphabet.charAt(r.nextInt(alphabet.length()));
+        securityString = String.valueOf(c1) + c2 + c3 + c4 + c5 + c6;
         return securityString;
     }
 
     //function check string is number or not
-    public static boolean isNumeric(String strNum) {
+    public static boolean IsNumeric(String strNum) {
         if (strNum == null) {
             return false;
         }
@@ -114,136 +124,144 @@ public class Asm01 {
         System.out.println("| NGAN HANG SO | FX18862@v1.0.0         |");
         System.out.println("+-----------+-----------------+---------+");
         System.out.println("| 1. Nhap CCCD                          |");
-        System.out.println("| 0 Thoat                               |");
+        System.out.println("| 0. Thoat                              |");
         System.out.println("+-----------+-----------------+---------+");
         //Loop for main program
-        boolean doProgram1 = true;                          // Logic for program can run or stop
-        while (doProgram1) {
-            int numEnter;
+        while (true) {
+            String stringEnter;                 // Create a string input then use Try-Catch convert to number ADVANTAGE
             System.out.println(" ");
             System.out.print("Chuc nang: ");
-            numEnter = scanner.nextInt();
-            if (numEnter == 1) {
-                int numRandom = Create_Random_Number();
-                System.out.println("Nhap ma xac thuc: " + numRandom);
-                //Loop for check random security number
-                boolean doProgram2 = true;                                  // Logic for program can run or stop
-                while (doProgram2) {
-                    int numEnterCheck;
-                    numEnterCheck = scanner.nextInt();
-                    if (numEnterCheck == numRandom) {
-                        //Loop for check string number
-                        boolean doProgram3 = true;                          // Logic for program can run or stop
-                        System.out.print("Vui long nhap so can cuoc cong dan: ");
-                        while (doProgram3) {
-                            String numCCCD = scanner.next();                // Number user enter for ID
-                            // Exit program
-                            if (numCCCD.equals("No")) {
-                                doProgram2 = false;
-                                break;
-                            }
-                            //Check length of string
-                            boolean lengthCCCD;
-                            if (numCCCD.length() == 12) {
-                                lengthCCCD = true;
-                            } else {
-                                lengthCCCD = false;
-                            }
-                            //Check string is a number ?
-                            boolean isNumber = isNumeric(numCCCD);
-                            //Analysis string number
-                            // Variable for check
-                            String numCity = "";
-                            String numGender = "";
-                            String numCentury = "";
-                            String numEnd = "";
-                            // Variable for print to screen
-                            String BirthYear = "";
-                            String Gender = "";
-                            //Check city number
-                            try {
-                                if (isNumber && lengthCCCD) {
-                                    //Set value for variable to check
-                                    numCity = String.valueOf(numCCCD.charAt(0))
-                                            + numCCCD.charAt(1)
-                                            + numCCCD.charAt(2);
-                                    numGender = String.valueOf(numCCCD.charAt(3));
-                                    numCentury = String.valueOf(numCCCD.charAt(4))
-                                            + numCCCD.charAt(5);
-                                    numEnd = String.valueOf(numCCCD.charAt(6))
-                                            + numCCCD.charAt(7)
-                                            + numCCCD.charAt(8)
-                                            + numCCCD.charAt(9)
-                                            + numCCCD.charAt(10)
-                                            + numCCCD.charAt(11);
-                                    // Check city
-                                    if (!(dict1.get(numCity) == null)) {
-                                        // Choose birth
-                                        switch (numGender) {
-                                            case "0", "1" -> BirthYear = "19" + numCentury;
-                                            case "2", "3" -> BirthYear = "20" + numCentury;
-                                            case "4", "5" -> BirthYear = "21" + numCentury;
-                                            case "6", "7" -> BirthYear = "22" + numCentury;
-                                            case "8", "9" -> BirthYear = "23" + numCentury;
-                                            default -> {
+            stringEnter = scanner.nextLine();
+            try {
+                int numEnter = Integer.valueOf(stringEnter);
+                if (numEnter == 1) {
+                    String numRandom = CreateStringNumber();
+                    System.out.println("Nhap ma xac thuc: " + numRandom);
+                    //Loop for check random security number
+                    boolean doProgram2 = true;                                      // Logic for program can run or stop
+                    while (doProgram2) {
+                        String numEnterCheck;
+                        numEnterCheck = scanner.nextLine();
+                        if (Objects.equals(numEnterCheck, numRandom)) {
+                            //Loop for check string number
+                            boolean doProgram3 = true;                              // Logic for program can run or stop
+                            System.out.print("Vui long nhap so can cuoc cong dan: ");
+                            while (doProgram3) {
+                                String numCCCD = scanner.next();                    // Number user enter for ID
+                                // Exit program
+                                if (numCCCD.equals("No")) {
+                                    doProgram2 = false;
+                                    break;
+                                }
+                                //Check length of string
+                                boolean lengthCCCD;
+                                if (numCCCD.length() == 12) {
+                                    lengthCCCD = true;
+                                } else {
+                                    lengthCCCD = false;
+                                }
+                                //Check string is a number ?
+                                boolean isNumber = IsNumeric(numCCCD);
+                                //Analysis string number
+                                // Variable for check
+                                String numCity = "";
+                                String numGender = "";
+                                String numCentury = "";
+                                String numEnd = "";
+                                // Variable for print to screen
+                                String BirthYear = "";
+                                String Gender = "";
+                                //Check city number
+                                try {
+                                    if (isNumber && lengthCCCD) {
+                                        //Set value for variable to check
+                                        numCity = String.valueOf(numCCCD.charAt(0))
+                                                + numCCCD.charAt(1)
+                                                + numCCCD.charAt(2);
+                                        numGender = String.valueOf(numCCCD.charAt(3));
+                                        numCentury = String.valueOf(numCCCD.charAt(4))
+                                                + numCCCD.charAt(5);
+                                        numEnd = String.valueOf(numCCCD.charAt(6))
+                                                + numCCCD.charAt(7)
+                                                + numCCCD.charAt(8)
+                                                + numCCCD.charAt(9)
+                                                + numCCCD.charAt(10)
+                                                + numCCCD.charAt(11);
+                                        // Check city
+                                        if (!(dict1.get(numCity) == null)) {
+                                            // Choose birth
+                                            switch (numGender) {
+                                                case "0", "1" -> BirthYear = "19" + numCentury;
+                                                case "2", "3" -> BirthYear = "20" + numCentury;
+                                                case "4", "5" -> BirthYear = "21" + numCentury;
+                                                case "6", "7" -> BirthYear = "22" + numCentury;
+                                                case "8", "9" -> BirthYear = "23" + numCentury;
+                                                default -> {
+                                                }
                                             }
-                                        }
-                                        // Choose gender
-                                        switch (numGender) {
-                                            case "0", "2", "4", "6", "8" -> Gender = "Nam";
-                                            case "1", "3", "5", "7", "9" -> Gender = "Nu";
-                                            default -> {
+                                            // Choose gender
+                                            switch (numGender) {
+                                                case "0", "2", "4", "6", "8" -> Gender = "Nam";
+                                                case "1", "3", "5", "7", "9" -> Gender = "Nu";
+                                                default -> {
+                                                }
                                             }
                                         }
                                     }
+                                } catch (NullPointerException ex) {
+                                    System.out.println(dict1.get(numCity));
+                                    System.out.println("Error null");
+                                    System.out.println(dict1.get(numCity) == null);
                                 }
-                            } catch (NullPointerException ex) {
-                                System.out.println(dict1.get(numCity));
-                                System.out.println("Error null");
-                                System.out.println(dict1.get(numCity) == null);
-                            }
-                            //Check all condition of string number
-                            if (isNumber && lengthCCCD && (!(dict1.get(numCity) == null))) {
-                                PrintScreen();
-                                while (true) {
-                                    int numCheckFunc = scanner.nextInt();
-                                    if (numCheckFunc == 0) {
-                                        System.out.println("Thoat chuong trinh kiem tra");
-                                        doProgram3 = false;
-                                        doProgram2 = false;
-                                        break;
-                                    } else if (numCheckFunc == 1) {
-                                        System.out.println("Noi sinh: " + dict1.get(numCity));
-                                        PrintScreen();
-                                    } else if (numCheckFunc == 2) {
-                                        System.out.println("Gioi tinh: " + Gender + " | " + BirthYear);
-                                        PrintScreen();
-                                    } else if (numCheckFunc == 3) {
-                                        System.out.println("So ngau nhien: " + numEnd);
-                                        PrintScreen();
-                                    } else {
-                                        System.out.println("");
-                                        System.out.println("Please enter again number");
-                                        PrintScreen();
+                                //Check all condition of string number
+                                if (isNumber && lengthCCCD && (!(dict1.get(numCity) == null))) {
+                                    while (true) {
+                                        String stringCheckFunc = scanner.nextLine();
+                                        try {
+                                            int numCheckFunc = Integer.valueOf(stringCheckFunc);
+                                            if (numCheckFunc == 0) {
+                                                System.out.println("Thoat chuong trinh kiem tra");
+                                                doProgram3 = false;
+                                                doProgram2 = false;
+                                                break;
+                                            } else if (numCheckFunc == 1) {
+                                                System.out.println("Noi sinh: " + dict1.get(numCity));
+                                                PrintScreen();
+                                            } else if (numCheckFunc == 2) {
+                                                System.out.println("Gioi tinh: " + Gender + " | " + BirthYear);
+                                                PrintScreen();
+                                            } else if (numCheckFunc == 3) {
+                                                System.out.println("So ngau nhien: " + numEnd);
+                                                PrintScreen();
+                                            } else {
+                                                System.out.println("");
+                                                PrintScreen();
+                                            }
+                                        } catch (NumberFormatException e) {
+                                            System.out.println("");
+                                            PrintScreen();
+                                        }
                                     }
+                                } else {
+                                    System.out.println(" ");
+                                    System.out.println("So can cuoc cong dan khong hop le");
+                                    System.out.print("Vui long nhap lai hoac 'No' de thoat: ");
                                 }
-                            } else {
-                                System.out.println(" ");
-                                System.out.println("So can cuoc cong dan khong hop le");
-                                System.out.print("Vui long nhap lai hoac 'No' de thoat: ");
                             }
+                        } else {
+                            System.out.println(" ");
+                            System.out.println("Ma xac thuc khong dung. Vui long thu lai.");
+                            System.out.print("Nhap ma xac thuc: ");
                         }
-                    } else {
-                        System.out.println(" ");
-                        System.out.println("Ma xac thuc khong dung. Vui long thu lai.");
-                        System.out.print("Nhap ma xac thuc: ");
                     }
+                } else if (numEnter == 0) {
+                    System.out.println(" ");
+                    System.out.println("Program is shutting down");
+                    break;
+                } else {
+                    System.out.println("Invalid number! Please enter again");
                 }
-            } else if (numEnter == 0) {
-                System.out.println(" ");
-                System.out.println("Program is shutting down");
-                doProgram1 = false;
-            } else {
+            } catch (NumberFormatException e) {
                 System.out.println("Invalid number! Please enter again");
             }
         }
