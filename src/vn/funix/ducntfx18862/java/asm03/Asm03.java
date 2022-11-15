701c3dfa33ba02d71fbbe0076a5d77622cea8fc8
lux01123 <nguyenduc27315@gmail.com>
2022-11-15 10:02:56 +0700

    ASM 03 This is first commit

---
+++ src/vn/funix/ducntfx18862/java/asm03/Asm03.java
@@ -0,0 +1,4 @@
+package vn.funix.ducntfx18862.java.asm03;
+
+public class Asm03 {
+}
---
+++ src/vn/funix/ducntfx18862/java/asm03/models/SavingsAccount.java
@@ -0,0 +1,6 @@
+package vn.funix.ducntfx18862.java.asm03.models;
+
+import vn.funix.ducntfx18862.java.asm02.models.Account;
+
+public class SavingsAccount extends Account {
+}
